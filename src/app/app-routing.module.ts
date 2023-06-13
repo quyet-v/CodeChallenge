42a1daf883cfbd2536e3965bfd45a68626342fad
quyet-v <72632257+quyet-v@users.noreply.github.com>
2023-06-13 13:12:09 +1200

    feature: generated cart page,items,item, and checkout components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { LoginComponent } from './modules/auth/components/login/login.component'
 import { HomeComponent } from './components/home/home.component';
 import { authGuard } from './guards/auth.guard';
 import { ProductsComponent } from './modules/products/components/products/products.component';
+import { CartComponent } from './modules/cart/components/cart/cart.component';
 
 const routes: Routes = [
   {path: "login", component: LoginComponent},
   {path: "", component: HomeComponent, canActivate: [authGuard]},
-  {path: "products", component: ProductsComponent, canActivate: [authGuard]}
+  {path: "products", component: ProductsComponent, canActivate: [authGuard]},
+  {path: "cart", component: CartComponent, canActivate: [authGuard]}
+
 
 ];
 
