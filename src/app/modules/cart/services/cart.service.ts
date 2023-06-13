b4ef69841cab57ba7059d1488a828f42dbb3dc56
quyet-v <72632257+quyet-v@users.noreply.github.com>
2023-06-13 13:42:14 +1200

    feature: implemented getCart, findIndex, and removeProduct for cart service

--- src/app/modules/cart/services/cart.service.ts
+++ src/app/modules/cart/services/cart.service.ts
@@ -13,4 +13,19 @@ export class CartService {
   addProduct(product: Product) {
     this.cart.push(product);
   }
+
+  getCart(): Product[] {
+    return this.cart;
+  }
+
+  findIndex(product: Product): number {
+    return this.cart.findIndex(p => p.sku == product.sku);
+  }
+
+  removeProduct(product: Product) {
+    const index = this.findIndex(product);
+    if(index > -1) {
+      this.cart.splice(index,1);
+    }
+  }
 }
